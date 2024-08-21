ec78214c4d6fa578392eb13165166d3d1ef85cba
윤여진 <duwls0349@naver.com>
2024-08-21 11:48:18 +0900

    useEffect Practice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("I run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("i run only once.");
+  };
+  useEffect(iRunOnlyOnce, []);
+  // useEffect(() => {
+  //   console.log("CALL THE API ONCE");
+  // }, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!</h1>
-      <Button text="Continue" />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me!</button>
     </div>
   );
 }
