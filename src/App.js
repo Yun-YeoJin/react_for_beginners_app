4eca6905998e77b7a89363fd1246e7746f118db3
윤여진 <duwls0349@naver.com>
2024-08-21 13:27:07 +0900

    useEffect Practice more

--- src/App.js
+++ src/App.js
@@ -4,18 +4,31 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("I run all the time");
-  const iRunOnlyOnce = () => {
-    console.log("i run only once.");
-  };
-  useEffect(iRunOnlyOnce, []);
-  // useEffect(() => {
-  //   console.log("CALL THE API ONCE");
-  // }, []);
+
+  useEffect(() => {
+    console.log("CALL THE API ONCE");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length >= 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]); //if keyword가 변환될 때만 실행되게 하기.
+  useEffect(() => {
+    console.log("counter is Changed");
+  }, [counter]);
   return (
     <div>
-      <h1>{counter}</h1>
+      <input
+        value={keyword} //value가 change될 때만 검색하고 싶음.
+        onChange={onChange}
+        type="text"
+        placeholder="Search Here..."
+      />
+      <h1>Welcome To MPS {counter}</h1>
       <button onClick={onClick}>Click me!</button>
     </div>
   );
