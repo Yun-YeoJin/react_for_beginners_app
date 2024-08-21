e46d6e2a13a6f8e0e9143991eedfdde90545cf35
윤여진 <duwls0349@naver.com>
2024-08-21 15:30:39 +0900

    To Do List Part One

--- src/App.js
+++ src/App.js
@@ -4,64 +4,32 @@ import { useEffect, useState } from "react";
 import { func } from "prop-types";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("I run all the time");
-
-  useEffect(() => {
-    console.log("CALL THE API ONCE");
-  }, []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length >= 5) {
-      console.log("SEARCH FOR", keyword);
+  const [toDo, setTodo] = useState("");
+  const [toDos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
     }
-  }, [keyword]); //if keyword가 변환될 때만 실행되게 하기.
-  useEffect(() => {
-    console.log("counter is Changed");
-  }, [counter]);
+    setTodo("");
+    setTodos((currentArray) => [toDo, ...currentArray]); //... : 배열의 요소 가져오기
+  };
+  console.log(toDos);
   return (
     <div>
-      <input
-        value={keyword} //value가 change될 때만 검색하고 싶음.
-        onChange={onChange}
-        type="text"
-        placeholder="Search Here..."
-      />
-      <h1>Welcome To MPS {counter}</h1>
-      <button onClick={onClick}>Click me!</button>
+      <h1>My To Do List ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
 
-function Hello() {
-  // function destroyedFn() {
-  //   console.log("Bye!!!");
-  // }
-  // function effectFn() {
-  //   console.log("Created!!!");
-  //   return destroyedFn;
-  // }
-  useEffect(function () {
-    console.log("Created!!!");
-    return function () {
-      console.log("Bye!!!!");
-    };
-  }, []);
-  return <h1>Hello</h1>;
-}
-
-function App2() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-  return (
-    <div>
-      {showing ? <Hello /> : null}
-
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-    </div>
-  );
-}
-
-export default App2;
+export default App;
