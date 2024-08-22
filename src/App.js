2446448f644d5c0e2bf2826f7123d7d3f08675ff
윤여진 <duwls0349@naver.com>
2024-08-22 14:59:25 +0900

    Parameters and Publishing Practice

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
     <Router>
       <Routes>
         <Route path="/hello" element={<h2>Hello Everybody</h2>} />
-        <Route path="/movie" element={<Detail />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<Detail />} />{" "}
+        {/* id는 받을 변수명 지정 */}
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
       </Routes>
     </Router>
   );
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,13 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-function Movie({ coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
       <h2>
         {/*페이지가 Reload 되지 않게 하기 위한 Link to*/}
-        <Link to="/movie">{title}</Link>
+        <Link to={`/movie/${id}`}>{title}</Link> {/* 홑따옴표 주의 */}
       </h2>
       <p>{summary}</p>
       <ul>
@@ -22,6 +22,7 @@ function Movie({ coverImg, title, summary, genres }) {
 }
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
